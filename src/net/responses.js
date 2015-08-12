b7de33acb835bcdb06e7a2c2f6f66e9ecd54781d
Jake Voytko <jakevoytko@gmail.com>
2015-08-12 15:02:18 -0400

    Adds a util for collecting chunks in a request into a single buffer.

---
+++ src/net/responses.js
@@ -0,0 +1,25 @@
+/**
+ * Contains a node.js module with utility methods for dealing with responses.
+ * TODO(jake): Test.
+ */
+
+var http = require('http');
+
+
+/**
+ * Joins all of the chunks together for the given request. Assumes that the
+ * request is going to 'end' at some point; this is not appropriate for use with
+ * streams like hanging-GETs.
+ */
+var collectBuffer = function(
+  result: http.IncomingMessage, callback: (data: Buffer) => void) {
+
+  var data = [];
+  result.on('data', function(chunk) {
+    data.push(chunk);
+  });
+  result.on('end', function() {
+    callback(Buffer.concat(data));
+  });
+};
+exports.collectBuffer = collectBuffer;
